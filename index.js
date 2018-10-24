697989de34170872b7e3c25e726e77e44adf9c28
ShadowBlade <maxence.beaumont6@gmail.com>
2018-10-24 12:45:57 +0200

    asd

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.get('/load', function(req, res) {
 app.post('/retrievePlayerData', async function(req, res){
   var connection    = await connect();
   var requestResult = await retrieveFromDataBase(connection.db, "Datas", req.body['playerID']);
-
+  console.log(req.body['playerID']);
   connection.db.close();
   var response;
   if (requestResult.length)
