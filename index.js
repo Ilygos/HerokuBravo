a56b76c9a14910c36e814667af5e243aa1cfd4f7
ShadowBlade <maxence.beaumont6@gmail.com>
2018-10-24 14:44:03 +0200

    asdas

--- index.js
+++ index.js
@@ -23,11 +23,11 @@ app.post('/retrievePlayerData', async function(req, res){
   var response;
   if (requestResult.length)
   {
-  response = requestResult.result[0]['playerID'] + "."
-   + requestResult.result[0]['softcurrency'] + "."
-   + requestResult.result[0]['hardcurrency']+ "."
-   + requestResult.result[0]['xpearned']+ "."
-   + requestResult.result[0]['energy'];
+    response = requestResult.result[0]['playerID'] + "."
+    + requestResult.result[0]['softcurrency'] + "."
+    + requestResult.result[0]['hardcurrency']+ "."
+    + requestResult.result[0]['xpearned']+ "."
+    + requestResult.result[0]['energy'];
   }
   else {
     response = ""
@@ -38,56 +38,56 @@ app.post('/retrievePlayerData', async function(req, res){
 });
 
 app.get("/retrieveLevels", async function(req, res){
-    var connection    = await connect();
-    var requestResult = await retrieveFromDataBase(connection.db, "Levels");
+  var connection    = await connect();
+  var requestResult = await retrieveFromDataBase(connection.db, "Levels");
 
-    connection.db.close();
-    res.send(requestResult.result[0]['levels']);
+  connection.db.close();
+  res.send(requestResult.result[0]['levels']);
 });
 
 function retrieveFromDataBase(db, collectionName){
-    return new Promise(resolve => {
-        db.db("tacticalbravo2018").collection(collectionName).find().toArray(async function(err, result){
-            resolve({err:err, result:result});
-        });
-    })
+  return new Promise(resolve => {
+    db.db("tacticalbravo2018").collection(collectionName).find().toArray(async function(err, result){
+      resolve({err:err, result:result});
+    });
+  })
 }
 
 
 function retrievePlayerData(db, collectionName, playerIDStr)
 {
-    return new Promise(
-      resolve => {
-        var query = {playerID:playerIDStr};
-        db.db('tacticalbravo2018').collection(collectionName).find(query).toArray(async function(err, result){
-          resolve({err:err, result:result});
-        })
-      }
-    )
+  return new Promise(
+    resolve => {
+      var query = {playerID:playerIDStr};
+      db.db('tacticalbravo2018').collection(collectionName).find().toArray(async function(err, result){
+        resolve({err:err, result:result});
+      })
+    }
+  )
 }
 
 function connect() {
   return new Promise(resolve => {
-      mongoDB.connect(url, { useNewUrlParser: true }, function(err, db) {
-        resolve({err:err, db:db});
+    mongoDB.connect(url, { useNewUrlParser: true }, function(err, db) {
+      resolve({err:err, db:db});
     });
   });
 }
 
 
 app.post('/uplevels', function(req, res) {
-    var form = req.body;
-    console.log(form['levels']);
-    res.send( form['levels']);
-    PushDatabase(req.body, "Levels");
+  var form = req.body;
+  console.log(form['levels']);
+  res.send( form['levels']);
+  PushDatabase(req.body, "Levels");
 });
 
 app.post('/upPlayerData', function(req, res) {
-    console.log(req.body);
-    var form = req.body;
-    var obj = {playerID: form['playerID'], softcurrency: form['softcurrency'], hardcurrency: form['hardcurrency'], xpearned: form['xpearned'], energy: form['energy']};
-    PushDatas(obj, "Datas");
-    res.send("Hello c'est push !");
+  console.log(req.body);
+  var form = req.body;
+  var obj = {playerID: form['playerID'], softcurrency: form['softcurrency'], hardcurrency: form['hardcurrency'], xpearned: form['xpearned'], energy: form['energy']};
+  PushDatas(obj, "Datas");
+  res.send("Hello c'est push !");
 });
 
 app.listen(port, () => {
@@ -97,28 +97,28 @@ app.listen(port, () => {
 function PushDatas(obj, collectionName)
 {
   MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  var dbo = db.db("tacticalbravo2018");
-  dbo.createCollection(collectionName);
-  dbo.collection(collectionName).insertOne(obj, function(err, res) {
     if (err) throw err;
-    console.log("1 document inserted");
-    db.close();
+    var dbo = db.db("tacticalbravo2018");
+    dbo.createCollection(collectionName);
+    dbo.collection(collectionName).insertOne(obj, function(err, res) {
+      if (err) throw err;
+      console.log("1 document inserted");
+      db.close();
+    });
   });
-});
 }
 
 function PushDatabase(obj, collectionName)
 {
   MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  var dbo = db.db("tacticalbravo2018");
-  dbo.collection(collectionName).drop();
-  dbo.createCollection(collectionName);
-  dbo.collection(collectionName).insertOne(obj, function(err, res) {
     if (err) throw err;
-    console.log("1 document inserted");
-    db.close();
+    var dbo = db.db("tacticalbravo2018");
+    dbo.collection(collectionName).drop();
+    dbo.createCollection(collectionName);
+    dbo.collection(collectionName).insertOne(obj, function(err, res) {
+      if (err) throw err;
+      console.log("1 document inserted");
+      db.close();
+    });
   });
-});
 }
