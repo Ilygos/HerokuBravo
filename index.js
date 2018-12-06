3aa60fee1c63e206fe03eaa53a125f40c5b78222
ShadowBlade <maxence.beaumont6@gmail.com>
2018-12-06 12:34:26 +0100

    Killian Test 3

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.get("/retrieveLevels", async function(req, res){
   var requestResult = await retrieveFromDataBase(connection.db, "Levels");
 
   connection.db.close();
-  console.log(requestResult.result);
+  console.log(requestResult.result['levels']);
   res.send(requestResult.result['levels']);
 });
 
